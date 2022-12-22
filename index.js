1f2538f4931e39df4bf99f2ef0a69839378c2547
uddinmayraj <mayrajuddin03@gmail.com>
2022-12-23 02:02:56 +0600

    adding user

--- index.js
+++ index.js
@@ -3,22 +3,27 @@ const cors = require('cors');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
-
-
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('donate blood server')
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6hrxcma.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
     try {
-
+        const usersCollection = client.db("donateBlood").collection("users");
         // create user 
         app.post('/users', async (req, res) => {
             const user = req.body
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
         })
     }
     catch { }
